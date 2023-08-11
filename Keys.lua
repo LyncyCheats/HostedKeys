6c3371bc47cbe42023982232657ac611b4fe065a
LyncyCheats <142054193+LyncyCheats@users.noreply.github.com>
2023-08-11 17:21:48 -0300

    Create Keys.lua

---
+++ Keys.lua
@@ -0,0 +1,8 @@
+return {
+    keyExpiration = os.time{year=2024, month=8, day=31},
+    whitelistedKeys = {
+        {key = "key1", maxUses = 1},
+        {key = "CHAVE2", maxUses = 3},
+        {key = "CHAVE3", maxUses = 10}
+    }
+}
