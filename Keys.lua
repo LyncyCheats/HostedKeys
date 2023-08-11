f6b22d729bbfe211d3542e29b4521008698927db
LyncyCheats <142054193+LyncyCheats@users.noreply.github.com>
2023-08-11 17:52:30 -0300

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,7 +1,7 @@
 return {
     keyExpiration = os.time{year=2024, month=8, day=31},
     whitelistedKeys = {
-        {key = "Trial"},
+        {key = "key1", maxUses = 99999999999999999999999999},
         {key = "CHAVE2", maxUses = 3},
         {key = "CHAVE3", maxUses = 10}
     }
